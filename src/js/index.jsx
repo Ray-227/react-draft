80d85e1942d7aa215dbf5b39556b49d794672e4c
Ray <ray-227@ya.ru>
2021-02-27 03:21:35 +0300

    Завершена подготовка для написания сайта на React.
    eslint работает на данный момент, только с обычными js файлами, нужно будет
    сделать и под React.

---
+++ src/js/index.jsx
@@ -0,0 +1,9 @@
+import '@/styles/styles.scss';
+
+import React from 'react';
+import {render} from 'react-dom';
+
+import App from '../components/App/index.jsx';
+
+render(<App />, document.getElementById('app'));
+
